da6027032720b1c9b3beb33159d80b51b2d4ec7d
Murodjon <Murodjon@users.noreply.github.com>
2021-12-12 01:03:51 +0500

    Initial Commit

---
+++ Saxovat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Saxovat
+//
+//  Created by Shonazarov Zavqiddin   on 12/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
